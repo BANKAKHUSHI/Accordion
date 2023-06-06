82a4fbc5e69a5a9ac25d805a6e9c207cb9a55c04
BANKAKHUSHI <105860690+BANKAKHUSHI@users.noreply.github.com>
2023-06-06 23:13:30 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,23 @@
+export const question =[
+    {
+    id: 1,
+    question:'what is the virtual DOM',
+    answer:'DOM stand for document object model.the DOM REPRESENT AN HTML'
+},{
+    id: 2,
+    question:'what is the virtual DOM',
+    answer:'DOM stand for document object model.the DOM REPRESENT AN HTML'
+},{
+    id: 3,
+    question:'what is the virtual DOM',
+    answer:'DOM stand for document object model.the DOM REPRESENT AN HTML'
+},{
+    id: 4,
+    question:'what is the virtual DOM',
+    answer:'DOM stand for document object model.the DOM REPRESENT AN HTML'
+},{
+    id: 5,
+    question:'what is the virtual DOM',
+    answer:'DOM stand for document object model.the DOM REPRESENT AN HTML'
+},
+]
